5a1aa7a820a15d99c277f02b11c67c18b31b4378
supanut9 <supanut.tanso@gmail.com>
2025-06-30 00:14:25 +0700

    feat(algorithm): add buble sort

---
+++ algorithm/sorting/bubble.js
@@ -0,0 +1,141 @@
+// A sorting algorithm where the largest values bubble up to the top!
+
+// 1st version
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP!
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+// 2nd version ( readability )
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+
+  return arr;
+}
+
+// 3nd version ( Optimized with noSwaps )
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwaps = false;
+      }
+    }
+
+    if (noSwaps) break;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3]));
+
+// Challenge bubbleSort with comparator function callback
+function bubbleSort(arr, comparator) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  const defaultComparator = (a, b) => {
+    return a - b;
+  };
+
+  comparator =
+    typeof comparator !== 'function' ? defaultComparator : comparator;
+
+  let noSwaps;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (comparator(arr[j], arr[j + 1]) > 0) {
+        swap(arr, j, j + 1);
+        noSwaps = false;
+      }
+    }
+
+    if (noSwaps) break;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([4, 20, 12, 10, 7, 9]));
+console.log(bubbleSort([0, -10, 7, 4]));
+console.log(bubbleSort([1, 2, 3]));
+console.log(bubbleSort([]));
+
+var nums = [
+  4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342,
+  32,
+];
+console.log(bubbleSort(nums));
+
+var kitties = ['LilBub', 'Garfield', 'Heathcliff', 'Blue', 'Grumpy'];
+function strComp(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log(bubbleSort(kitties, strComp));
+
+var moarKittyData = [
+  {
+    name: 'LilBub',
+    age: 7,
+  },
+  {
+    name: 'Garfield',
+    age: 40,
+  },
+  {
+    name: 'Heathcliff',
+    age: 45,
+  },
+  {
+    name: 'Blue',
+    age: 1,
+  },
+  {
+    name: 'Grumpy',
+    age: 6,
+  },
+];
+
+function oldestToYoungest(a, b) {
+  return b.age - a.age;
+}
+
+console.log(bubbleSort(moarKittyData, oldestToYoungest));
