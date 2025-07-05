5d0ffa839471cd0c7f500f841c45ce1c272f67d0
supanut9 <supanut.tanso@gmail.com>
2025-07-05 17:33:43 +0700

    feat(leetcode): add 912 Sort an Array
    with heap sort ( the best sort equal to radix )

---
+++ leetcode/912/heap-sort.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+function heapify(arr, n, i) {
+  let largest = i;
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]]; // Swap
+
+    heapify(arr, n, largest);
+  }
+}
+
+function sortArray(arr) {
+  const n = arr.length;
+
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
+  }
+
+  return arr;
+}
